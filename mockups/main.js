2269932e83b2843cebd374edd56f25b541731c73
Scott Klein <scotty.klei@gmail.com>
2019-02-02 17:14:49 +1100

    Merged layout with the new basic login and sign-up components.

--- mockups/main.js
+++ mockups/main.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-/*This component is used to login.
-It returns nothing and fires no events
-*/
-Vue.component('login-form', {
-    template: `
-    <div class="loginform">
-          <p>Username:</p>
-          <p>Password:</p>              
-    </div>
-    `
-})
-
-=======
->>>>>>> 2b49b376d465b450f084f1fc104f9f8bbe7dd047
 var app = new Vue({
     el: '#app'
 })
