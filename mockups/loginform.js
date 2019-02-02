0d9ba45c5cfd567e912c0fdf22782bc20bacc0fa
Scott Klein <scotty.klei@gmail.com>
2019-02-02 22:39:41 +1100

    Styled main page mostly

--- mockups/loginform.js
+++ mockups/loginform.js
@@ -1,21 +1,37 @@
 Vue.component('login-form', {
-    template: `
-    <div class="loginform">
-        
-        <!-- our sign in form -->
-        <form id=""loginform>
-            <!-- user name -->
-                <div class="field">
-                    <label class="lable">User name:</lable>
-                    <input type="text" class="input" name="name">
-                </div>
-                
-            <!-- password -->
-                <div class="field">
-                    <lable class="lable">Password:</lable>
-                    <input type="password" class="input" name="name">
-                </div>
-        </form>
-    </div>
+    template: 
     `
+    <div class="login-form">
+    <p></p>
+    <button @click="logIn">Sign in</button>
+        <div v-show="LogInClicked">
+            <!-- our sign in form -->
+            <form id="loginform">
+                
+                <!-- user name -->
+                    <div class="field">
+                        <label class="lable">User name:</lable>
+                        <input type="text" class="input" name="name">
+                    </div>
+                
+                <!-- password -->
+                    <div class="field">
+                        <lable class="lable">Password:</lable>
+                        <input type="password" class="input" name="name">
+                    </div>
+            </form>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            LogInClicked: false
+        }
+    },
+    methods: {
+        logIn() {
+            this.LogInClicked = true;
+
+        }
+    }
 })
