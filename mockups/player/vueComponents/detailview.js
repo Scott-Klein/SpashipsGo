8f3be3dff38702c4a884ea9115d493d30b76d9c9
Scott Klein <scotty.klei@gmail.com>
2019-02-03 20:34:47 +1100

    player home page menu navigation prototype working.

--- mockups/player/vueComponents/detailview.js
+++ mockups/player/vueComponents/detailview.js
@@ -1,16 +1,98 @@
-Vue.component('detail-view.vue', {
+Vue.component('detail-view', {
     props: {
         selectedView: String
     },
-    template: 
+    template:
         `
         <div>
-            <div></div>
+
+            <div id="overviewDetail" class="detailPanel" v-show="showOverview">
+                <h1>overview</h1>
+                <ul>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                    <li>overview dummy information.</li>
+                </ul>
+            </div>
+
+            <div id="buidlingDetail" class="detailPanel" v-show="showBuilding">
+                <h1>building</h1>
+                <p> A grid showing the buildings that can be built to go here.</p>
+                <p> Built buildings should be visible on the larger panel to the right designated for graphics.</p>
+                <p> Built buildings can also be upgraded to improve their performance. </p>
+            </div>
+
+            <div id="researchDetail" class="detailPanel" v-show="showResearch">
+                <h1>research</h1>
+                <p> a research tree should go here, to unlock new abilities and buildings.</p>
+            </div>
+
+            <div id="fleetDetail" class="detailPanel" v-show="showFleet">
+                <h1>fleet</h1>
+                <p> somewhere to summon and control fleets </p>
+                <p> spaceships can be built here </p>
+            </div>
+
+            <div id="StarDetail" class="detailPanel" v-show="showStar">
+                <h1>star</h1>
+                <p> this will be a grid of planets and celestial bodies in grahpical form </p>
+                <p> selecting player owned planets will result in that planet becoming focused.
+            </div>
         </div>
         `,
     data() {
         return {
             testVariable: false
+        }
+    },
+    computed: {
+        showOverview(){
+            if(this.selectedView == "overview"){
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        showBuilding(){
+            if(this.selectedView == "building"){
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        showResearch(){
+            if(this.selectedView == "research"){
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        showFleet(){
+            if(this.selectedView == "fleet")
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        showStar(){
+            if(this.selectedView == "star")
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 })
--- mockups/player/vueComponents/main.js
+++ mockups/player/vueComponents/main.js
@@ -1,6 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isHidden:true
+        isHidden:true,
+        viewSelection: "overview"
+    },
+    methods: {
+        viewOverview(){
+            this.viewSelection = "overview";
+        },
+        viewBuildings(){
+            this.viewSelection = "building";
+        },
+        viewResearch(){
+            this.viewSelection = "research";
+        },
+        viewFleet(){
+            this.viewSelection = "fleet";
+        },
+        viewStars(){
+            this.viewSelection = "star"
+        }
     }
+    
 })
