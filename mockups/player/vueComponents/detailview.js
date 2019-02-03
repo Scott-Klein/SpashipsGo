aa1da1d4ab48ce37be7f53fadeffbd0f1885fb03
Scott Klein <scotty.klei@gmail.com>
2019-02-03 14:55:43 +1100

    detailView component work has begun. component scaffold is finished

---
+++ mockups/player/vueComponents/detailview.js
@@ -0,0 +1,16 @@
+Vue.component('detail-view.vue', {
+    props: {
+        selectedView: String
+    },
+    template: 
+        `
+        <div>
+            <div></div>
+        </div>
+        `,
+    data() {
+        return {
+            testVariable: false
+        }
+    }
+})
