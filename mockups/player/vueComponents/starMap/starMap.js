d5a8752164b2b052068dd0b9ce09772618ea237a
Shox <shoxie12345@gmail.com.au>
2019-02-09 23:04:05 +1100

    issue with planet images

--- mockups/player/vueComponents/starMap/starMap.js
+++ mockups/player/vueComponents/starMap/starMap.js
@@ -2,25 +2,46 @@ Vue.component('building-star', {
     template:
         `   
         <div>
-        <h1> 
             <div class="center">
-                <p>Universe</p>
+                <h1>Universe</h1>
             </div> 
-        </h1> 
-        
-        
-            <div class="box"> </div>
+            <div class="box">
+                <ul class="planetList">
+                    <li v-for="planet in planets">
+                        <planet :planetList="planets.number"></planet>
+                    </li>
+                </ul>   
+            </div>
         </div>
               
         `,
     data() {
         return {
-            test: {
-                return: true
-            }
+            planets: [
+                { number: '1' },
+                { number: '2' },
+                { number: '3' },
+                { number: '4' },
+                { number: '5' },
+                { number: '6' },
+            ]
         }
-
     }
+})
 
 
+Vue.component('planet', {
+    props: {
+        planetList: String
+    },
+    template: `
+        <div>
+            <img :src=planetIcon>
+        </div>
+    `,
+    computed: {
+        planetIcon() {
+            return "vueComponents/starMap/planetImages/" + this.planetList + ".svg";
+        }
+    }
 })
