6126f32e5e3ca5c4052611cdf1874e1a893d4c9c
shoxieje <47261937+shoxieje@users.noreply.github.com>
2019-02-26 12:20:43 +1100

    Update researchQuest.js

--- mockups/player/research/researchQuest.js
+++ mockups/player/research/researchQuest.js
@@ -2,19 +2,19 @@ Vue.component('research-script', {
     template: `
     <div id="researchQuest" class="quest">  
         <research-box title="Quest 1">
-            <input type="button" value="Finding fuel" class="container" onClick="quest1_no1()">     
+            <input type="button" value="Finding fuel" class="container" v-on:click="quest1_no1()">     
             </input>
-            <input type="button" value="Finding fire" class="container" onClick="quest1_no2()" >     
+            <input type="button" value="Finding fire" class="container" v-on:click="quest1_no2()" >     
             </input>
         </research-box>
 
         <research-box title="Quest 2">
-            <input type="button" value="Finding water" class="container" onClick="quest2_no1()">     
+            <input type="button" value="Finding water" class="container" v-on:click="quest2_no1()">     
             </input>
         </research-box>
 
         <research-box title="Quest 3">
-            <input type="button" value="Finding oil" class="container" onClick="quest3_no1()">     
+            <input type="button" value="Finding oil" class="container" v-on:click="quest3_no1()">     
             </input>
         </research-box>
     </div>
